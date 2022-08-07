cf334915b95c5fd66b371f42a1f5b5cc0d2e9d5a
Neo D. Truman <neodtruman@gmail.com>
2022-08-07 21:10:48 +0700

    Chapter 5.1

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,31 @@
+import { Fragment, useEffect, useState } from "react";
+
 export default function AllPostsPage() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('/posts.json')
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data.posts);
+        setIsLoading(false);
+      })
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <h1>All Posts</h1>
+    <Fragment>
+      <h1>All Posts</h1>
+      <ul>
+        {posts.map(post =>
+          <li key={post.id}>{post.title}</li>
+        )}
+      </ul>
+    </Fragment>
   );
 }
