db9950e40d4ef521af7e6ac01dfc9b6ea27e4cef
Neo D. Truman <neodtruman@gmail.com>
2022-08-07 21:51:43 +0700

    Chapter 5.2.1.3a

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,35 @@
-import { useRouter } from "next/router";
+import path from 'path';
+import fs from 'fs/promises';
 
-export default function PostDetailPage() {
-  const router = useRouter();
-  const postId = router.query.id;
-
+export default function PostDetailPage(props) {
+  const { post } = props;
   return (
-    <h1>Detail Page - Post {postId}</h1>
+    <h1>Detail Page - {post.title}</h1>
   );
+}
+
+export async function getStaticProps(context) {
+  const postId = context.params.id;
+
+  const filePath = path.join(process.cwd(), 'public', 'posts.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const post = data.posts.find(p => p.id === postId);
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '1' } },
+      { params: { id: '2' } },
+      { params: { id: '3' } }
+    ],
+    fallback: false
+  }
 }
