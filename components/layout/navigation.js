5001022467574eeba1a4503b5dba053b03eb1c5d
Neo D. Truman <neodtruman@gmail.com>
2022-08-07 21:05:35 +0700

    Chapter 4

---
+++ components/layout/navigation.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import styles from './navigation.module.css';
+
+export default function Navigation() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles['nav-link']}>HOME</a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">
+              <a className={styles['nav-link']}>Posts</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/user/login">
+              <a className={styles['nav-link']}>Sign In</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ pages/user/login.js
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+  return (
+    <h1>Log in to Your Account</h1>
+  );
+}
