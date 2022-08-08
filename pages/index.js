c4e2b3782da40e48b07ae718f605c81d031e7e94
Neo D. Truman <neodtruman@gmail.com>
2022-08-08 11:18:23 +0700

    Chapter 5.2.2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-export default function Home() {
+import { Fragment } from "react";
+
+export default function Home(props) {
   return (
-    <h1>Home Page</h1>
+    <Fragment>
+      <h1>Home Page</h1>
+      <div>{props.message}</div>
+    </Fragment>
   )
+}
+
+export async function getServerSideProps() {
+  console.log('getServerSideProps called')
+  return {
+    props: {
+      message: 'This is a test message'
+    }
+  }
 }
