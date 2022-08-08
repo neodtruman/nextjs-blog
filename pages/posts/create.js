ebbf30c7001a4c0c17618193f5aebb3adae835a8
Neo D. Truman <neodtruman@gmail.com>
2022-08-08 15:54:58 +0700

    Chapter 7.4.1

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,6 +1,27 @@
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
 import NewPostForm from "../../components/posts/new-post-form";
 
 export default function CreatePostPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then(session => {
+      if (!session) {
+        router.replace('/user/login');
+      }
+      else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <NewPostForm />
   );
