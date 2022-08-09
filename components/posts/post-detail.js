203891fa1521bee92ea7d64a5e1376f209b76b92
Neo D. Truman <neodtruman@gmail.com>
2022-08-09 17:03:24 +0700

    Chapter 10.4

--- components/posts/post-detail.js
+++ components/posts/post-detail.js
@@ -1,10 +1,15 @@
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import vs from 'react-syntax-highlighter/dist/cjs/styles/prism/vs';
 
 import Image from 'next/image';
 import { Fragment } from 'react';
 import styles from './post-detail.module.css';
+
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+SyntaxHighlighter.registerLanguage('css', css);
 
 export default function PostDetail({ post }) {
   return (
@@ -32,7 +37,7 @@ export default function PostDetail({ post }) {
           const { className, children } = code;
           const lang = className.replace('language-', '');
           return (
-            <SyntaxHighlighter language={lang} showLineNumbers="true" wrapLongLines="true" style={vs} customStyle={{ "overflow": "hidden", "background-color": "#fafafa" }} >
+            <SyntaxHighlighter language={lang} showLineNumbers="true" wrapLongLines="true" style={vs} customStyle={{ "overflow": "hidden", "backgroundColor": "#fafafa" }} >
               {children}
             </SyntaxHighlighter>
           )
